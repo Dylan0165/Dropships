599e4164eb80c9f06788360586fb4fbe65b274a8
Claumi <mombanzaclaumi@gmail.com>
2026-07-29 13:06:37 +0200

    auto: 20260729-130637

--- UIcontrol/src/server/design/components/assemble.ts
+++ UIcontrol/src/server/design/components/assemble.ts
@@ -14,6 +14,7 @@ import { baseCss } from './base-css.js'
 import { getComponent } from './registry.js'
 import { BRAND_TOKEN } from './chrome.js'
 import { motionCss, motionRuntime, type MotionProfile } from '../anime-presets.js'
+import { resolveHeroVisual, type HeroVisual } from '../hero-visual.js'
 
 export interface AssembleInput {
   dna: DesignDNA
